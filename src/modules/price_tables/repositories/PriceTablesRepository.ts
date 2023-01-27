4d0a5d88f29c61135c0e61b1babedeff7c078cd3
Newton Duarte <newton_@hotmail.com>
2023-01-27 07:08:30 -0300

    feat: add price table type orm repository implementation

---
+++ src/modules/price_tables/repositories/PriceTablesRepository.ts
@@ -0,0 +1,38 @@
+import { getRepository, Repository } from "typeorm";
+import { ICreatePriceTableDTO } from "../dtos/ICreatePriceTableDTO";
+import { PriceTable } from "../entities/PriceTable";
+
+export class PriceTablesRepository {
+  private priceTablesRepository: Repository<PriceTable>;
+
+  constructor() {
+    this.priceTablesRepository = getRepository(PriceTable);
+  }
+
+  async find(): Promise<PriceTable[]> {
+    return this.priceTablesRepository.find();
+  }
+
+  async findById(id: string): Promise<PriceTable> {
+    return this.priceTablesRepository.findOne({ where: { id }, relations: ['price_table_products'] });
+  }
+
+  async create({ id, name, products, created_at }: ICreatePriceTableDTO): Promise<PriceTable> {
+    console.log(products);
+    const priceTable = this.priceTablesRepository.create({
+      id,
+      name,
+      price_table_products: products,
+      created_at: created_at || new Date(),
+      updated_at: new Date(),
+    });
+
+    await this.priceTablesRepository.save(priceTable);
+
+    return priceTable;
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.priceTablesRepository.delete(id);
+  }
+}
