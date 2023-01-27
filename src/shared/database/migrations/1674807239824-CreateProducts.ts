6abf728cbf7af05470c662b881345f42e6a8f3a8
Newton Duarte <newton_@hotmail.com>
2023-01-27 07:09:32 -0300

    chore: add create products type orm migration

---
+++ src/shared/database/migrations/1674807239824-CreateProducts.ts
@@ -0,0 +1,63 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateProducts1674807239824 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(
+        new Table({
+          name: 'products',
+          columns: [
+            {
+              name: 'id',
+              type: 'uuid',
+              isPrimary: true,
+            },
+            {
+              name: 'name',
+              type: 'varchar',
+            },
+            {
+              name: 'price',
+              type: 'decimal',
+              precision: 15,
+              scale: 2,
+            },
+            {
+              name: 'unit_id',
+              type: 'uuid',
+            },
+            {
+              name: 'created_at',
+              type: 'timestamp',
+              default: 'now()',
+            },
+            {
+              name: 'updated_at',
+              type: 'timestamp',
+              default: 'now()',
+            },
+            {
+              name: 'deleted_at',
+              type: 'timestamp',
+              isNullable: true,
+            },
+          ],
+          foreignKeys: [
+            {
+              name: 'ProductUnit',
+              referencedTableName: 'units',
+              referencedColumnNames: ['id'],
+              columnNames: ['unit_id'],
+              onDelete: 'CASCADE',
+              onUpdate: 'CASCADE',
+            },
+          ],
+        })
+      );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('products');
+    }
+
+}
